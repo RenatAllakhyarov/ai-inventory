afc37c67d4bc622a03c1bbb47af8dcc551a77f88
RenatAllakhyarov <allakhyarovrenat@gmail.com>
2026-09-21 19:57:42 +0300

    fix: Shared toast close icon

--- src/components/ToastItem/index.tsx
+++ src/components/ToastItem/index.tsx
@@ -1,5 +1,6 @@
 import { DEFAULT_TOAST_DURATION_MS } from "@utils/constants";
 import { useEffect, type ReactElement } from "react";
+import CloseIcon from "../CloseIcon";
 import { type ToastMessage, type ToastType } from "../../types";
 import "./style.css";
 
@@ -60,21 +61,7 @@ const ToastItem = ({ toast, onDismiss }: IToastItemProps): ReactElement => {
                 }}
                 aria-label="Закрыть уведомление"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="18"
-                    height="18"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    aria-hidden="true"
-                >
-                    <path d="M18 6 6 18" />
-                    <path d="m6 6 12 12" />
-                </svg>
+                <CloseIcon />
             </button>
         </article>
     );
